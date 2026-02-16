8588ae0adfe74647c75a4a8a69994b32cedf0030
Mohammed Usman <196224424+musman-uk@users.noreply.github.com>
2026-02-16 19:02:58 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
 
   // Load instrument notes
   eleventyConfig.addCollection("notes", function (collection) {
-    return collection.getFilteredByGlob("source/contents/instruments/**/*.md");
+    return collection.getFilteredByGlob("source/content/instruments/**/*.md");
   });
 
   return {
